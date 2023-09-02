ae3fa495d1e1b8520854703503900719dd4fcbb2
mutara-tech <mutarawilliam@outlook.com>
2023-09-02 17:13:46 -0400

    Update App.java

--- HelloJava/src/main/App.java
+++ HelloJava/src/main/App.java
@@ -3,10 +3,10 @@ package main;
 public class App {
     public static void main(String[] args) throws Exception {
 
-      byte a = 1;
+      byte a = 1; // Initialization
       short b = 2;
       int c = 3;
-      long d = 4l;
+      long d = 4_000_000_000l;
       System.out.println(a + b + c + d);
 
       float e = 5.0f;
@@ -17,6 +17,11 @@ public class App {
       String h = "Hello world";
       System.out.println(g);
       System.out.println(h);
+
+      boolean i = true;
+      boolean j = false;
+      System.out.println(i);
+      System.out.println(j);
 
       String escapeSequences = "\\t, \\n, \\r, \\s, \\', \\";
       System.out.println(escapeSequences);
